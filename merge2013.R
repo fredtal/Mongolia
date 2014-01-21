dd490c8d3651ddabf63479a4f9569dce51a2675c
Chris Free <cfree14@gmail.com>
2014-01-21 14:02:38 -0500

    Creation of 2013 merge code

---
+++ merge2013.R
@@ -0,0 +1,14 @@
+# Merge 2013 data
+
+# Define directory
+datadir <- "~cfree/Dropbox/Mongolia data merge project/2013"
+
+# Read formatted individual data
+data <- read.csv(paste(datadir, "2013_mongolia_data_cmf.csv", sep="/"))
+
+# Read formatted Mendee morphometric data
+morphs <- read.csv(paste(datadir, "2013_mendee_data_cmf.csv", sep="/"))
+
+# Read formatted Talia isotope data
+isotopes <- read.csv(paste(datadir, "2013_isotope_data_cmf.csv", sep="/"))
+
